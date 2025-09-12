efe36eec85aac6c95f701c1eb392782f15500f63
Jorge Gustavo Pérez Fuentevilla <gustave.loof@gmail.com>
2025-09-12 18:58:01 +0200

    Remove matlab_msg_gen from tracking
    Carpeta que se genera cuando se construyen los mensajes custom de ROS2 en MATLAB desde main.m

--- MATLAB_ws/custom/matlab_msg_gen/glnxa64/build/custom_interfaces/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gustavo-fuentevilla/DefectsExp_UR/MATLAB_ws/custom/matlab_msg_gen/glnxa64/src/custom_interfaces
-# Build directory: /home/gustavo-fuentevilla/DefectsExp_UR/MATLAB_ws/custom/matlab_msg_gen/glnxa64/build/custom_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
